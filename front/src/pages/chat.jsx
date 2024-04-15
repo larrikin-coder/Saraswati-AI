750a297b1fdd8d4753b7db70c137348d9e411bf2
kaushal sengupta <kaushalsubho2005@gmail.com>
2024-04-15 23:47:15 +0530

    some touches

--- front/src/pages/chat.jsx
+++ front/src/pages/chat.jsx
@@ -4,38 +4,19 @@ import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 import SendIcon from '@mui/icons-material/Send';
-import axios from 'axios';
+//tujhe ml lena hai
 
 function Chat() {
     const [query, setQuery] = useState("");
     const [ml, setMl] = useState("");
 
-    // Update the query state whenever the input changes
     function prompt(event) {
         setQuery(event.target.value);
     }
-
-    // Update the ml state when the send button is clicked
     function send() {
         setMl(query);
+        setQuery("")
     }
-
-    // Use useEffect to send the ml state to the server when it updates
-    useEffect(() => {
-        if (ml) {
-            axios.post('http://127.0.0.1:5000/ml', { message: ml })
-            .then(response => {
-                // Handle the response here
-                console.log(response.data);
-            })
-            .catch(error => {
-                // Handle any errors here
-                console.error('There was an error!', error);
-            });
-        }
-    }, [ml]); // This effect depends on the ml state
-
-    // JSX code for the chat interface
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', pt: 60 }}>
             <TextField
@@ -44,6 +25,7 @@ function Chat() {
                 multiline
                 variant="outlined"
                 rows={2}
+                value={query}
                 onChange={prompt}
                 InputProps={{
                     endAdornment: (
